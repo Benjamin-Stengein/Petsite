41be83de8503c58bf9dfd6eb2eef9c8e0baab41a
Benjamin-Stengein <stengleinben@gmail.com>
2026-04-09 17:40:52 +0000

    Completed config and compat for shadcn components.

--- frontend/src/components/ui/button/Button.vue
+++ frontend/src/components/ui/button/Button.vue
@@ -3,7 +3,17 @@ import { Primitive } from "reka-ui";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from ".";
 
-const props = defineProps();
+const props = defineProps({
+  variant: { type: null, required: false },
+  size: { type: null, required: false },
+  class: {
+    type: [Boolean, null, String, Object, Array],
+    required: false,
+    skipCheck: true,
+  },
+  asChild: { type: Boolean, required: false },
+  as: { type: null, required: false, default: "button" },
+});
 </script>
 
 <template>
---
+++ frontend/src/lib/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { 
+  Card, 
+  CardContent, 
+  CardDescription, 
+  CardFooter, 
+  CardHeader, 
+  CardTitle,
+} from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+</script>
+<template>
+  <div class="min-h-screen bg-background text-foreground p-8 flex justify-center items-center">
+    
+    <Card class="w-[350px] shadow-xl">
+      <CardHeader>
+        <CardTitle>System Check</CardTitle>
+        <CardDescription>the website works!?</CardDescription>
+      </CardHeader>
+      
+      <CardContent>
+        <p class="text-sm">
+           can you see this
+           <p class="text-m">yesyesyes</p>
+        </p>
+      </CardContent>
+      
+      <CardFooter class="flex justify-between">
+        <Button variant="outline">yes?</Button>
+        <Button>yes!</Button>
+      </CardFooter>
+    </Card>
+
+  </div>
+`</template>
